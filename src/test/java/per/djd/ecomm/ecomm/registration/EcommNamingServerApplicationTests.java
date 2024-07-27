e96f68c05e67be4d277f51dc4b2cb772fbe70633
Dhruba Dawndawndhruba@gmail.com <dawndhruba@gmail.com>
2024-07-28 00:02:09 +0530

    Initial Commit

---
+++ src/test/java/per/djd/ecomm/ecomm/registration/EcommNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package per.djd.ecomm.ecomm.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
